5eed91d38d052d69c4d981a1dbbf69f648d4e951
beatcracker <beatcracker@users.noreply.github.com>
2020-07-17 03:39:30 +0300

    Update example scripts

--- examples/toptout_bash.sh
+++ examples/toptout_bash.sh
@@ -1,17 +1,27 @@
+#!/usr/bin/env bash
+
 # Homebrew
 # https://brew.sh
+
+# Usage data
 export HOMEBREW_NO_ANALYTICS='1'
 
 # AWS SAM CLI
 # https://aws.amazon.com/serverless/sam/
+
+# Usage data
 export SAM_CLI_TELEMETRY='0'
 
 # Azure CLI
 # https://docs.microsoft.com/en-us/cli/azure
+
+# Usage data
 export AZURE_CORE_COLLECT_TELEMETRY='0'
 
 # Google Cloud SDK
 # https://cloud.google.com/sdk
+
+# Usage data
 if command -v 'gcloud' >/dev/null 2>&1 >/dev/null
 then
   'gcloud' config set disable_usage_reporting true >/dev/null 2>&1
@@ -19,10 +29,14 @@ fi
 
 # Netdata
 # https://www.netdata.cloud
+
+# Usage data
 export DO_NOT_TRACK='1'
 
 # Netlify CLI
 # https://netlify.com
+
+# Usage data
 if command -v 'netlify' >/dev/null 2>&1 >/dev/null
 then
   'netlify' --telemetry-disable >/dev/null 2>&1
@@ -30,36 +44,82 @@ fi
 
 # Stripe CLI
 # https://stripe.com/docs/stripe-cli
+
+# Usage data
 export STRIPE_CLI_TELEMETRY_OPTOUT='1'
 
 # Tilt
 # https://tilt.dev
+
+# Usage data
 export DO_NOT_TRACK='1'
 
 # Apache Cordova CLI
 # https://cordova.apache.org
+
+# Usage data
 export CI='ANY_VALUE'
 
 # Gatsby
 # https://www.gatsbyjs.org
+
+# Usage data
 export GATSBY_TELEMETRY_DISABLED='1'
 
 # Hasura GraphQL engine
 # https://hasura.io
+
+# Usage data (CLI and Console)
 export HASURA_GRAPHQL_ENABLE_TELEMETRY='false'
 
 # .NET Core SDK
 # https://docs.microsoft.com/en-us/dotnet/core/tools/index
+
+# Usage data
 export DOTNET_CLI_TELEMETRY_OPTOUT='true'
 
 # Next.js
 # https://nextjs.org
+
+# Usage data
 export NEXT_TELEMETRY_DISABLED='1'
 
 # AutomatedLab
 # https://github.com/AutomatedLab/AutomatedLab
+
+# Usage data
 export AUTOMATEDLAB_TELEMETRY_OPTOUT='1'
 
 # PowerShell Core
 # https://github.com/powershell/powershell
+
+# Usage data
 export POWERSHELL_TELEMETRY_OPTOUT='1'
+
+# Firefox
+# https://www.mozilla.org/firefox/
+
+# Enable policies (macOS)
+# https://github.com/mozilla/policy-templates/tree/master/mac
+case "$OSTYPE" in
+  darwin*)
+    if command -v 'defaults' >/dev/null 2>&1 >/dev/null
+    then
+      'defaults' write /Library/Preferences/org.mozilla.firefox EnterprisePoliciesEnabled -bool TRUE >/dev/null 2>&1
+    fi
+  ;;
+esac
+
+# Firefox
+# https://www.mozilla.org/firefox/
+
+# Usage data
+# https://github.com/mozilla/policy-templates/blob/master/README.md
+case "$OSTYPE" in
+  darwin*)
+    if command -v 'defaults' >/dev/null 2>&1 >/dev/null
+    then
+      'defaults' write /Library/Preferences/org.mozilla.firefox DisableTelemetry -bool TRUE >/dev/null 2>&1
+    fi
+  ;;
+esac
--- examples/toptout_pwsh.ps1
+++ examples/toptout_pwsh.ps1
@@ -1,63 +1,135 @@
+#Requires -Version 5
+
+function Get-OsMoniker {
+    if ($IsCoreCLR) {
+        if ($IsWindows) {
+            'windows'
+        } elseif ($IsLinux) {
+            'linux'
+        } elseif ($IsMacOS) {
+            'macos'
+        }
+    } else {
+        'windows'
+    }
+}
+
 # Homebrew
 # https://brew.sh
+
+# Usage data
 ${env:HOMEBREW_NO_ANALYTICS} = '1'
 
 # AWS SAM CLI
 # https://aws.amazon.com/serverless/sam/
+
+# Usage data
 ${env:SAM_CLI_TELEMETRY} = '0'
 
 # Azure CLI
 # https://docs.microsoft.com/en-us/cli/azure
+
+# Usage data
 ${env:AZURE_CORE_COLLECT_TELEMETRY} = '0'
 
 # Google Cloud SDK
 # https://cloud.google.com/sdk
+
+# Usage data
 . {
-  & (Get-Command -Name 'gcloud' -CommandType Application -ErrorAction Stop)[0].Path --% config set disable_usage_reporting true
+  "& (Get-Command -Name 'gcloud' -CommandType Application -ErrorAction Stop)[0].Path --% config set disable_usage_reporting true"
 } *>&1 > $null
 
 # Netdata
 # https://www.netdata.cloud
+
+# Usage data
 ${env:DO_NOT_TRACK} = '1'
 
 # Netlify CLI
 # https://netlify.com
+
+# Usage data
 . {
-  & (Get-Command -Name 'netlify' -CommandType Application -ErrorAction Stop)[0].Path --% --telemetry-disable
+  "& (Get-Command -Name 'netlify' -CommandType Application -ErrorAction Stop)[0].Path --% --telemetry-disable"
 } *>&1 > $null
 
 # Stripe CLI
 # https://stripe.com/docs/stripe-cli
+
+# Usage data
 ${env:STRIPE_CLI_TELEMETRY_OPTOUT} = '1'
 
 # Tilt
 # https://tilt.dev
+
+# Usage data
 ${env:DO_NOT_TRACK} = '1'
 
 # Apache Cordova CLI
 # https://cordova.apache.org
+
+# Usage data
 ${env:CI} = 'ANY_VALUE'
 
 # Gatsby
 # https://www.gatsbyjs.org
+
+# Usage data
 ${env:GATSBY_TELEMETRY_DISABLED} = '1'
 
 # Hasura GraphQL engine
 # https://hasura.io
+
+# Usage data (CLI and Console)
 ${env:HASURA_GRAPHQL_ENABLE_TELEMETRY} = 'false'
 
 # .NET Core SDK
 # https://docs.microsoft.com/en-us/dotnet/core/tools/index
+
+# Usage data
 ${env:DOTNET_CLI_TELEMETRY_OPTOUT} = 'true'
 
 # Next.js
 # https://nextjs.org
+
+# Usage data
 ${env:NEXT_TELEMETRY_DISABLED} = '1'
 
 # AutomatedLab
 # https://github.com/AutomatedLab/AutomatedLab
+
+# Usage data
 ${env:AUTOMATEDLAB_TELEMETRY_OPTOUT} = '1'
 
 # PowerShell Core
 # https://github.com/powershell/powershell
+
+# Usage data
 ${env:POWERSHELL_TELEMETRY_OPTOUT} = '1'
+
+# Firefox
+# https://www.mozilla.org/firefox/
+
+# Enable policies (macOS)
+# https://github.com/mozilla/policy-templates/tree/master/mac
+switch (Get-OsMoniker) {
+  'macos' {
+    . {
+      "& (Get-Command -Name 'defaults' -CommandType Application -ErrorAction Stop)[0].Path --% write /Library/Preferences/org.mozilla.firefox EnterprisePoliciesEnabled -bool TRUE"
+    } *>&1 > $null
+  }
+}
+
+# Firefox
+# https://www.mozilla.org/firefox/
+
+# Usage data
+# https://github.com/mozilla/policy-templates/blob/master/README.md
+switch (Get-OsMoniker) {
+  'macos' {
+    . {
+      "& (Get-Command -Name 'defaults' -CommandType Application -ErrorAction Stop)[0].Path --% write /Library/Preferences/org.mozilla.firefox DisableTelemetry -bool TRUE"
+    } *>&1 > $null
+  }
+}
